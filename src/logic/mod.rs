32a506890164a04d0a191204f846c3a006fdfe68
Nikita <humb1t@yandex.ru>
2016-04-21 17:53:25 +0400

    Stub representation of app listener

--- src/logic/mod.rs
+++ src/logic/mod.rs
@@ -3,3 +3,29 @@ mod rules;
 mod lcm;
 mod physic;
 mod ai;
+
+pub struct Application {
+    pub name: &'static str,
+    pub platform: &'static str
+}
+
+pub trait ApplicationListener {
+    fn new(name: &'static str, platform: &'static str) -> Self;
+    fn name(&self) -> &'static str;
+    fn platform(&self) -> &'static str;
+
+    fn create(&self){
+        println!("Create application {} for platform {}",
+         self.name(), self.platform());
+    }
+
+    fn resize(&self, width: i32, height: i32){
+            println!("Resize to {}x{}",
+             width, height);
+    }
+
+    fn render(&self);
+    fn pause(&self);
+    fn resume(&self);
+    fn dispose(&self);
+}
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,55 @@ mod logic;
 mod net;
 mod utils;
 
+use logic::ApplicationListener;
+
+impl ApplicationListener for logic::Application {
+    fn new(name: &'static str, platform: &'static str) -> logic::Application {
+        logic::Application {name: name, platform: platform}
+    }
+
+    fn name(&self) -> &'static str {
+        self.name
+    }
+
+    fn platform(&self) -> &'static str {
+        self.platform
+    }
+
+    fn render(&self) {
+        println!("render");
+    }
+    fn pause(&self) {
+        println!("pause");
+    }
+    fn resume(&self) {
+        println!("resume");
+    }
+    fn dispose(&self) {
+        println!("dispose");
+    }
+}
+
+fn logger(text: &str)
+{
+    println!("LOG: {}", text);
+}
 
 fn main() {
-    println!("Hello, world!");
+    logger("start main");
+    let application: logic::Application =
+        ApplicationListener::new("Anduin", "desktop");
+        application.create();
+        'main: loop {
+            logger("start main loop");
+            for i in 0..5 {
+                let step = format!("{}", i);
+                logger(&step);
+                application.render();
+            }
+            logger("end main loop");
+            break 'main;
+        }
+        application.dispose();
+    logger("end main");
 }
