c5041aaee425f801c2465f7e3ee844c3deac4015
Nikita Puzankov <humb1t@yandex.ru>
2016-10-18 02:16:24 +0400

    autoformat and vulkan sample code moved to fn

--- tests/lib.rs
+++ tests/lib.rs
@@ -14,7 +14,7 @@ fn create_test_app() {
     println!("application created");
     let game_loop = lcm::GameLoop::new();
     println!("game_loop created");
-    application.input.add_input_processor(Box::new(anduin::InputProcessorStuct{}));
+    application.input.add_input_processor(Box::new(anduin::InputProcessorStuct {}));
     println!("add_input_processor finished");
     game_loop.run(&mut application);
     println!("game_loop runned");
@@ -38,14 +38,13 @@ fn create_game_loop() {
 
 #[test]
 fn create_simple_scene() {
-    let scene = core::scene::Stage {root: core::scene::Node::new("Root Node")};
+    let scene = core::scene::Stage { root: core::scene::Node::new("Root Node") };
     println!("Simple scene is created {:?}", scene);
 }
 
-fn create_simple_game()
-{
+fn create_simple_game() {
     let scene = core::scene::Stage {
-        root: core::scene::Node::build("Root Node", Actor{}, Control{}, Image{})
+        root: core::scene::Node::build("Root Node", Actor {}, Control {}, Image {}),
     };
     scene.update();
 }
@@ -74,23 +73,20 @@ impl Drawable for Image {
 }
 
 impl InputProcessor for Control {
-        fn key_down(&self, key: Key)
-        {
-            println!("Keypushed down: {:?}", key)
-        }
-    	fn key_up(&self, key: Key)
-        {
-            println!("Keypushed up: {:?}", key)
-        }
+    fn key_down(&self, key: Key) {
+        println!("Keypushed down: {:?}", key)
+    }
+    fn key_up(&self, key: Key) {
+        println!("Keypushed up: {:?}", key)
+    }
 }
 
-/*
-Simple game TC
-Game game = new Game(width, height, title);
-Screen menu_screen = new Screen(title);
-Button new_game = new Button();
-ButtonActionHandler start_game = new ButtonActionHandler(new_game);
-Stage main_stage = new Stage(new Viewport(new Camera()));
-main_stage.add(Ball{radius, Mesh{material, color}});
-main_stage.add(Line{vec![{x1,y1}, {x2,y2}]});
-*/
+// Simple game TC
+// Game game = new Game(width, height, title);
+// Screen menu_screen = new Screen(title);
+// Button new_game = new Button();
+// ButtonActionHandler start_game = new ButtonActionHandler(new_game);
+// Stage main_stage = new Stage(new Viewport(new Camera()));
+// main_stage.add(Ball{radius, Mesh{material, color}});
+// main_stage.add(Line{vec![{x1,y1}, {x2,y2}]});
+//
